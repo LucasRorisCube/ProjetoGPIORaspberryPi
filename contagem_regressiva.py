b4f3bf54b8362f781a7b54b2da5917a31773922e
LucasRorisCube <83036146+LucasRorisCube@users.noreply.github.com>
2023-10-04 16:26:56 -0300

    Update contagem_regressiva.py

--- contagem_regressiva.py
+++ contagem_regressiva.py
@@ -4,26 +4,26 @@ import time
 
 # Funcao para escrever a contagem na tela da maneira especificada
 def contagem(number):
-	div, rest = divmod(int(num), 60) # Acha a quantidade de minutos e segundos para contagem
+	min, sec = divmod(int(number), 60) # Acha a quantidade de minutos e segundos para contagem
 
 	# Conta ate chegar em zero
-	while ((div >= 0) and (rest >= 0)):
+	while ((min >= 0) and (sec >= 0)):
 
 		# Printa a contagem sempre na mesma linha
-		print(f'\rContagem regressiva ({div :02d}:{rest :02d})', end='') 
+		print(f'\rContagem regressiva ({min :02d}:{sec :02d})', end='') 
 		time.sleep(1) # Aguarda 1 segundo
-		rest -= 1 # Diminui a contagem
-		if rest < 0: # Caso os segundos acabem, reseta os segundos e diminui os minutos
-			rest = 59
-			div -= 1
+		sec -= 1 # Diminui a contagem dos segundos
+		if sec < 0: # Caso os segundos acabem, reseta os segundos e diminui os minutos
+			sec = 59
+			min -= 1
 #########################################
+
+pino_led = 5 # LED esta no pino 5
 
 # Inicializa a biblioteca de GPIO e desativa os Warnings de versão antiga
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(5, GPIO.OUT)
-
-pino_led = 5 # LED esta no pino 5
+GPIO.setup(pino_led, GPIO.OUT)
 
 # Desliga o LED
 GPIO.output(pino_led, GPIO.LOW)
