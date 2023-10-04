9ceae9c216472525aa5b821903347b7748f91e9d
LucasRorisCube <83036146+LucasRorisCube@users.noreply.github.com>
2023-10-04 16:17:04 -0300

    Update contagem_regressiva.py

--- contagem_regressiva.py
+++ contagem_regressiva.py
@@ -1,55 +1,57 @@
+# Importa as funcoes para manipulacao do tempo e dos pinos da Raspberry
 import RPi.GPIO as GPIO
 import time
 
-
-
+# Funcao para escrever a contagem na tela da maneira especificada
 def contagem(number):
-	div, rest = divmod(int(num), 60)
+	div, rest = divmod(int(num), 60) # Acha a quantidade de minutos e segundos para contagem
 
+	# Conta ate chegar em zero
 	while ((div >= 0) and (rest >= 0)):
-		
-		
-		print(f'\rContagem regressiva ({div :02d}:{rest :02d})', end='')
-		time.sleep(1)
-		rest -= 1
-		if rest < 0:
+
+		# Printa a contagem sempre na mesma linha
+		print(f'\rContagem regressiva ({div :02d}:{rest :02d})', end='') 
+		time.sleep(1) # Aguarda 1 segundo
+		rest -= 1 # Diminui a contagem
+		if rest < 0: # Caso os segundos acabem, reseta os segundos e diminui os minutos
 			rest = 59
 			div -= 1
-	print("\nAcendeu!")
-	GPIO.output(5, GPIO.HIGH) #ou usar True
-
-
 #########################################
 
-# Setup
+# Inicializa a biblioteca de GPIO e desativa os Warnings de versão antiga
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(5, GPIO.OUT)
 
-# Led no port 5
-GPIO.output(5, GPIO.LOW)
+pino_led = 5 # LED esta no pino 5
 
-# Obtencao do valor
+# Desliga o LED
+GPIO.output(pino_led, GPIO.LOW)
+
+# Obtencao do valor pelo usuario ate ser um valor valido
 value = 0
 while True:
 	try:
 		num = input("Digite o tempo da contagem em segundos: ")
 		value = int(num)
 
-		if value >= 0:
+		if value >= 0:	# Caso for valido, procede para contagem
 			break
 		else:
-			print('Positive Number please')
+			print('Digite um numero positivo por favor')
 	except ValueError:
-		print("Valid Number please")
+		print("Formato de numero invalido, tente novamente")
 
 # Realiza a contagem
 contagem(value)
+# Quando acaba a contagem,  acende o LED
+print("\nAcendeu!")
+GPIO.output(pino_led, GPIO.HIGH)
 
-# Espera com o led ligado
+# Espera com o led ligado ate uma interrupcao de teclado
 try:
 	while True:
 		pass
-
+# Limpa os pinos quando o programa acaba
 except KeyboardInterrupt:
 	GPIO.cleanup()
