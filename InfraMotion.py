35d4dc99aec8f2b351b360823a6184251cfeaf5d
LucasRorisCube <83036146+LucasRorisCube@users.noreply.github.com>
2023-10-04 15:41:40 -0300

    Update InfraMotion.py

--- InfraMotion.py
+++ InfraMotion.py
@@ -1,23 +1,27 @@
+# Importa as funcoes para manipulacao de leds e sensor de movimento
 from gpiozero import MotionSensor, LED
 from signal import pause
 
-
+# Funcao principal
 def main():
 	
-	
-	pir = MotionSensor(4)
-	led = LED(20)
+	sensor_of_motion = MotionSensor(4) # Define que o pino analogico do sensor de presenca esta no pino 4 da Raspberry
+	led = LED(20) # Define que o pino do LED esta no pino 20 da Raspberry
 
-	pir.when_motion = led.on
-	pir.when_no_motion = led.off
+	# Define que quando o sensor detecta movimento, o LED deve ligar
+	sensor_of_motion.when_motion = led.on
 
+	# Define que quando o sensor nao detecta movimento, o LED deve desligar
+	sensor_of_motion.when_no_motion = led.off
+
+	# Pausa o programa
 	pause()
-	
 
+# Deixa a funcao principal executando ate que seja detectado uma interrupcao do teclado
 try:
 	while True:
 		main()
-
+# Limpa os pinos quando o programa acaba
 except KeyboardInterrupt:
 	GPIO.cleanup()
 
